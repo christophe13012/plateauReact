e80c6a29f8dcfc39a473829b9c2d98e580cf9432
christophe13012 <45483049+christophe13012@users.noreply.github.com>
2019-06-07 10:16:26 +0200

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <h1 style={{ textAlign: "center", fontSize: 40 }}>Jeux tour par tour test</h1>
+    <h1 style={{ textAlign: "center", fontSize: 40 }}>Jeux tour par tour</h1>
   );
 };
 
