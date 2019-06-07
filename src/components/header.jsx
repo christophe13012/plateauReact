5b47861a595a90091a58a171a409f46d379acd9f
chris13012 <christophecaillet@icloud.com>
2019-06-07 10:14:57 +0200

    test

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const Header = () => {
   return (
-    <h1 style={{ textAlign: "center", fontSize: 40 }}>Jeux tour par tour</h1>
+    <h1 style={{ textAlign: "center", fontSize: 40 }}>
+      Jeux tour par tour test
+    </h1>
   );
 };
 
